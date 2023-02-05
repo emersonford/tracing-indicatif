adc499c92b3b1d6c6140a2eec75ede2ae080f8d2
Emerson Ford <emersontford@gmail.com>
2023-02-04 17:11:50 -0800

    add basic implementation
    Summary:
    add a minimum working example of spans triggering progress bars
    
    Test Plan:
    ```
    cargo run --examples basic
    ```

---
+++ examples/basic.rs
@@ -0,0 +1,43 @@
+use std::time::Duration;
+
+use futures::stream::{self, StreamExt};
+use rand::thread_rng;
+use rand::Rng;
+use tracing::info;
+use tracing::instrument;
+use tracing_indicatif::IndicatifLayer;
+use tracing_subscriber::layer::SubscriberExt;
+use tracing_subscriber::util::SubscriberInitExt;
+
+#[instrument]
+async fn do_work(val: u64) -> u64 {
+    let sleep_time = thread_rng().gen_range(Duration::from_millis(250)..Duration::from_millis(500));
+    tokio::time::sleep(sleep_time).await;
+
+    info!("doing work for val: {}", val);
+
+    let sleep_time = thread_rng().gen_range(Duration::from_millis(500)..Duration::from_millis(1000));
+    tokio::time::sleep(sleep_time).await;
+
+    val + 1
+}
+
+#[tokio::main]
+async fn main() {
+    let indicatif_layer = IndicatifLayer::new();
+    let indicatif_writer = indicatif_layer.get_writer();
+
+    tracing_subscriber::registry()
+        .with(indicatif_layer)
+        .with(tracing_subscriber::fmt::layer().with_writer(indicatif_writer))
+        .init();
+
+    let res: u64 = stream::iter((0..20).map(|val| do_work(val)))
+        .buffer_unordered(5)
+        .collect::<Vec<u64>>()
+        .await
+        .into_iter()
+        .sum();
+
+    println!("final result: {}", res);
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,121 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use std::io;
+use std::marker::PhantomData;
+use std::time::Duration;
+
+use indicatif::{MultiProgress, ProgressBar};
+use tracing_core::Subscriber;
+use tracing_core::span;
+use tracing_subscriber::fmt::MakeWriter;
+use tracing_subscriber::layer;
+use tracing_subscriber::registry::LookupSpan;
+
+// TODO(emersonford): find a cleaner way to integrate this layer with fmt::Layer.
+#[derive(Clone)]
+pub struct IndicatifWriter {
+    progress_bars: MultiProgress,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+impl io::Write for IndicatifWriter {
+    fn write(&mut self, buf: &[u8]) -> io::Result<usize> {
+        self.progress_bars.suspend(|| io::stderr().write(buf))
+    }
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn flush(&mut self) -> io::Result<()> {
+        self.progress_bars.suspend(|| io::stderr().flush())
+    }
+}
+
+impl<'a> MakeWriter<'a> for IndicatifWriter {
+    type Writer = IndicatifWriter;
+
+    fn make_writer(&'a self) -> Self::Writer {
+        self.clone()
+    }
+}
+
+pub struct IndicatifLayer<S> {
+    progress_bars: MultiProgress,
+    inner: PhantomData<S>,
+}
+
+impl<S> IndicatifLayer<S>
+where
+    S: Subscriber + for<'a> LookupSpan<'a>,
+{
+    pub fn new() -> Self {
+        Self {
+            progress_bars: MultiProgress::new(),
+            inner: PhantomData,
+        }
+    }
+
+    /// Returns the writer that should be passed into
+    /// [fmt::Layer::with_writer](tracing_subscriber::fmt::Layer::with_writer).
+    ///
+    /// This will result in all log messages being written to stderr and ensures the printing of
+    /// the log messages does not interfere with the progress bars.
+    pub fn get_writer(&self) -> impl for<'writer> MakeWriter<'writer> {
+        IndicatifWriter {
+            // `MultiProgress` is merely a wrapper over an `Arc`, so we can clone here.
+            progress_bars: self.progress_bars.clone(),
+        }
+    }
+}
+
+struct IndicatifSpanContext {
+    progress_bar: Option<ProgressBar>,
+}
+
+impl<S> layer::Layer<S> for IndicatifLayer<S>
+where
+    S: Subscriber + for<'a> LookupSpan<'a>,
+{
+    fn on_new_span(
+        &self,
+        _attrs: &span::Attributes<'_>,
+        id: &span::Id,
+        ctx: layer::Context<'_, S>,
+    ) {
+        let span = ctx
+            .span(id)
+            .expect("Span not found in context, this is a bug");
+        let mut ext = span.extensions_mut();
+
+        ext.insert(IndicatifSpanContext { progress_bar: None });
+    }
+
+    fn on_enter(&self, id: &span::Id, ctx: layer::Context<'_, S>) {
+        let span = ctx
+            .span(id)
+            .expect("Span not found in context, this is a bug");
+        let mut ext = span.extensions_mut();
+
+        if let Some(indicatif_ctx) = ext.get_mut::<IndicatifSpanContext>() {
+            // Start the progress bar when we enter the span for the first time.
+            indicatif_ctx.progress_bar.get_or_insert_with(|| {
+                let pb = self.progress_bars.add(ProgressBar::new_spinner());
+                pb.set_message(span.name());
+                pb.enable_steady_tick(Duration::from_millis(50));
+
+                pb
+            });
+        }
+    }
+
+    fn on_close(&self, id: span::Id, ctx: layer::Context<'_, S>) {
+        let span = ctx
+            .span(&id)
+            .expect("Span not found in context, this is a bug");
+        let mut ext = span.extensions_mut();
+
+        // Clear the progress bar only when the span has closed completely.
+        if let Some(pb) = ext
+            .get_mut::<IndicatifSpanContext>()
+            .and_then(|indicatif_ctx| indicatif_ctx.progress_bar.take())
+        {
+            pb.finish_and_clear();
+
+            self.progress_bars.remove(&pb);
+        }
     }
 }
