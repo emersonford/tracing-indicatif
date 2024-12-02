bce3db8b9a9edfdef3d95686e31064f3fb237ba3
Emerson Ford <emersontford@gmail.com>
2024-12-01 22:51:05 -0800

    use helper macros in examples

--- examples/stdout_stderr_printing.rs
+++ examples/stdout_stderr_printing.rs
@@ -1,12 +1,11 @@
-use std::io::Write;
 use std::time::Duration;
 
 use futures::stream::{self, StreamExt};
 use rand::thread_rng;
 use rand::Rng;
 use tracing::instrument;
-use tracing_indicatif::writer::get_indicatif_stderr_writer;
-use tracing_indicatif::writer::get_indicatif_stdout_writer;
+use tracing_indicatif::indicatif_eprintln;
+use tracing_indicatif::indicatif_println;
 use tracing_indicatif::IndicatifLayer;
 use tracing_subscriber::layer::SubscriberExt;
 use tracing_subscriber::util::SubscriberInitExt;
@@ -16,19 +15,8 @@ async fn do_work(val: u64) -> u64 {
     let sleep_time = thread_rng().gen_range(Duration::from_millis(250)..Duration::from_millis(500));
     tokio::time::sleep(sleep_time).await;
 
-    writeln!(
-        get_indicatif_stderr_writer().unwrap(),
-        "writing val {} to stderr",
-        val
-    )
-    .unwrap();
-
-    writeln!(
-        get_indicatif_stdout_writer().unwrap(),
-        "writing val {} to stdout",
-        val
-    )
-    .unwrap();
+    indicatif_eprintln!("writing val {} to stderr", val);
+    indicatif_println!("writing val {} to stdout", val);
 
     let sleep_time =
         thread_rng().gen_range(Duration::from_millis(500)..Duration::from_millis(1000));
